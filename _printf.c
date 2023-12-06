e147f257b8a48d919fc60573d89e60f808540ecd
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-06 20:20:05 +0300

    created _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * _printf - my printf
+ * @str: char pointer
+ *
+ * Return: void
+ */
+
+void _printf(const char *str)
+{
+  write(STDOUT_FILENO, str, _strlen(str));
+}
