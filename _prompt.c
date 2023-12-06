cf4b121385398668600c51a4a5f403fe7bc7b16f
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-06 20:44:31 +0300

    _prompt.c

---
+++ _prompt.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * _prompt - Display the shell prompt
+ *
+ * Description:
+ * This function is responsible for displaying the prompt for the shell,
+ * indicating that the shell is ready to accept user input. In this case,
+ * the prompt is "_shell$".
+ */
+
+void _prompt(void)
+{
+  _printf("_shell$");
+}
