c5d4f81e17af19e95acf3357c5e264aecb365f58
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-07 11:59:01 +0300

    Update read_command.c

--- read_command.c
+++ read_command.c
@@ -1,5 +1,34 @@
 #include "shell.h"
 
+/**
+ * read_command - Read a command from the user
+ * @c: Buffer to store the user input
+ * @size: Size of the buffer
+ *
+ * Description:
+ * This function reads a command from the user using fgets, and handles
+ * possible errors, such as reaching the end of the input or encountering
+ * a read error.
+ *
+ * Parameters:
+ * @c: Buffer to store the user input.
+ * @size: Size of the buffer.
+ */
+
 void read_command(char *c, size_t size)
 {
+  if(fgets(c, size, stdin) == NULL)
+  {
+    if(feof(stdin))
+    {
+      _printf("\n");
+      exit(EXIT_SUCCESS);
+    }
+    else
+    {
+      _printf("Error reading user input.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  c[strcspn(c, "\n")] = '\0';
 }
