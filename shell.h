6f5d564bb7ac7120fec978e3f5db35780a04250d
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-07 19:13:35 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -12,9 +12,11 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define MAX_INPUT_SIZE 1024
+
 void _printf(const char *str);
 void _prompt(void);
-void exec_command(const char *c);
+void exec_command(const char *command);
 void read_command(char *c, size_t size);
 
 #endif /* SHELL_H */
