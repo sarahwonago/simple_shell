6bd363cbebc45f0f3b6eda9d1c2cb22daf2c3996
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-07 19:12:51 +0300

    Update exec_command.c

--- exec_command.c
+++ exec_command.c
@@ -2,44 +2,68 @@
 
 /**
  * exec_command - Execute a shell command
- * @c: The command to be executed
+ * @command: The command to be executed
  *
  * Description:
- * This function takes a command as input, forks a new process to execute
- * the command, and waits for the child process to complete.
+ * This function takes a command as input, searches for the executable
+ * file in the directories listed in the PATH environment variable,
+ * forks a new process to execute the command, and waits for the child
+ * process to complete.
  *
  * Parameters:
- * @c: The command to be executed, provided as a string.
+ * @command: The command to be executed, provided as a string.
  */
 
-void exec_command(const char *c)
+void exec_command(const char *command)
 {
-  pid_t child_pid = fork();
+    pid_t child_pid = fork();
 
-  if(child_pid == -1)
-  {
-    perror("fork");
-    exit(EXIT_FAILURE);
-  }
-  else if(child_pid == 0)
-  {
-    char *args[128];
-    int count = 0;
-
-    char *token = strtok((char *)c, " ");
-    while(token != NULL)
+    if (child_pid == -1)
     {
-      args[count++] = token;
-      token = strtok(NULL, " ");
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
-    args[count] = NULL;
+    else if (child_pid == 0)
+    {
+        char *args[128];
+        int count = 0;
 
-    execvp(args[0], args);
-    _printf("Error executing command.\n");
-    exit(EXIT_FAILURE);
-  }
-  else
-  {
-    wait(NULL);
-  }
-} 
+        char *token = strtok((char *)command, " ");
+        while (token != NULL) {
+            args[count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[count] = NULL;
+
+        if (access(args[0], F_OK) == -1)
+        {
+            char *path = getenv("PATH");
+            char *path_copy = strdup(path);
+            char *dir = strtok(path_copy, ":");
+
+            while (dir != NULL)
+            {
+                char path_buffer[PATH_MAX];
+                snprintf(path_buffer, PATH_MAX, "%s/%s", dir, args[0]);
+                if (access(path_buffer, F_OK) == 0)
+                {
+                    execv(path_buffer, args);
+                }
+                dir = strtok(NULL, ":");
+            }
+
+            _printf("Command not found: %s\n", args[0]);
+            free(path_copy);
+            exit(EXIT_FAILURE);
+        }
+
+        execv(args[0], args);
+      
+        perror("execv");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(NULL);
+    }
+}
