d1240e134c78a5849f0e49be6534fb9da3081ba0
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-15 16:50:37 +0300

    get_cwd.c

---
+++ get_cwd.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * get_cwd - prints working directory.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int get_cwd(void)
+{
+    char buf[1024];
+    char *cwd;
+
+    if (getcwd(buf, sizeof(buf)) == NULL)
+    {
+        perror("getcwd");
+        return (1);
+    }
+
+    printf("Current working directory: %s\n", buf);
+
+    return (0);
+}
