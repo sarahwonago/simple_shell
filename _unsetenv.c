4a01f790a60a6ad6fb53cd1a28ca7ac0e239b316
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-15 16:43:01 +0300

    Create _unsetenv.c

---
+++ _unsetenv.c
@@ -0,0 +1,33 @@
+#include "environment.h"
+
+/**
+ * _unsetenv - Delete a variable from the environment.
+ *
+ * This function deletes a variable with the specified name from the environment.
+ *
+ * @name: The name of the variable to delete.
+ *
+ * Return: 0 on success, -1 on failure.
+ */
+int _unsetenv(const char *name)
+{
+    extern char **environ;
+    char **env = environ;
+    char **p;
+
+    if (name == NULL || *name == '\0')
+        return (-1);
+
+    while (*env != NULL)
+    {
+        if (strncmp(*env, name, strlen(name)) == 0 && (*env)[strlen(name)] == '=')
+        {
+            for (p = env; *p != NULL; p++)
+                *p = *(p + 1);
+            return (0);
+        }
+        env++;
+    }
+
+    return (-1);
+}
