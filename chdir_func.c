ba3b7ac363c6d13be2d6cc5fb8e505bb36f5082a
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-15 16:45:59 +0300

    chdir_func.c

---
+++ chdir_func.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * chdir_func - Changes working directory and prints it.
+ * @path: The path of the directory to change to.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int chdir_func(const char *path)
+{
+    if (chdir(path) == -1)
+    {
+        perror("chdir");
+        return (1);
+    }
+
+    printf("Changed current working directory to: %s\n", path);
+    return (0);
+}
