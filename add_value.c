292e12688e90583fd8003bf98cd9b86104ee361c
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-12-19 14:28:05 +0300

    Update add_value.c

--- add_value.c
+++ add_value.c
@@ -7,6 +7,7 @@
  *
  * Return: Pointer to the new string, or NULL if memory allocation fails.
  */
+
 char *add_value(char *key, char *value)
 {
     unsigned int l1, l2, a, b;
@@ -15,6 +16,7 @@ char *add_value(char *key, char *value)
     l1 = _strlen(key);
     l2 = _strlen(value);
     new = malloc(sizeof(char) * (l1 + l2 + 2));
+    
     if (new == NULL)
         return (NULL);
     for (a = 0; key[a] != '\0'; a++)
@@ -23,5 +25,6 @@ char *add_value(char *key, char *value)
     for (b = 0; value[b] != '\0'; b++)
         new[a + 1 + b] = value[b];
     new[a + 1 + b] = '\0';
+    
     return (new);
 }
